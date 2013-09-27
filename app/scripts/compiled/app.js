4e8cc134a545b2e1975b53b8861d80ace8c2de5b
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-09-27 15:39:59 -0400

    HTML generation API for MS

--- app/scripts/compiled/app.js
+++ app/scripts/compiled/app.js
@@ -1,4 +1,6 @@
-(function() {return define([],function() {
-return "here we are";
+(function() {return define(["./html"],function(h) {
+"here we are";
+var data = {title:"'Allo, 'Allo!",msg:"You now have",components:["HTML5 Boilerplate","jQuery","RequireJS","Mocha-Script Runtime","Mocha-Script Compiler","Bacon.js"]};
+return ($("#welcome")).html(h.collect(h.h1(data.title),h.p(data.msg),h.ul(map(h.li,data.components))));
 });
 })();
--- app/scripts/compiled/html.js
+++ app/scripts/compiled/html.js
@@ -1,5 +1,33 @@
-(function() {return define(["com.tantaman.util.overload"],function(overload) {
-return {div:function(overload,jsarray,Object,jsarray,Array,String,fn,attrs,content,jsarray,Object,fn,attrs,.div,this,attrs,null,jsarray,jsarray,Array,String,fn,content,.div,this,null,content) {
+(function() {return define(function() {
+var isContent = function(attrs) {
+return or(((typeof attrs)==="string"),(attrs instanceof Array));
+};
+var normalizeAttrs = function(content,attrs) {
+return ((attrs===undefined) ? {} : attrs)
+;
+};
+var normalizeContent = function(content) {
+return ((content instanceof Array) ? (content).join("") : content)
+;
+};
+var attrString = function(attrs) {
+return (map(function(v,k) {
+return plus.call(null, k,"=\"",v,"\"");
+},attrs)).join(" ");
+};
+var createElemFn = function(type) {
+return function(content,attrs) {
+(attrs = normalizeAttrs(content,attrs));
+(content = normalizeContent(content));
+return plus.call(null, "<",type,attrString(attrs),">",content,"</",type,">");
+};
+};
+var api = {collect:function() {
+return (Array.prototype.slice.call(arguments,0)).join("");
 }};
+foreach(["div","li","ul","ol","p","span","article","section","i","b","strong","a","buton","input","textarea","select","option","h1","h2","h3","h4"],function(elem) {
+return ((api)[elem] = createElemFn(elem));
+});
+return api;
 });
 })();
