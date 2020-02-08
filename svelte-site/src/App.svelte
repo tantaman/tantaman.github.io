9e7d12ea6c6951a842764fee6df72e972ed4b71b
tantaman <matt.crinklaw@gmail.com>
2020-02-08 15:04:35 -0500

    bg component

--- svelte-site/src/App.svelte
+++ svelte-site/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
   import { Router, Route } from "svelte-routing";
   import Home from "./Home.svelte";
+  import ProceduarlBG from "./ProceduralBG.svelte";
 
   export let url = "";
 </script>
 
+<ProceduarlBG />
 <Router {url}>
   <Route path="/">
     <Home />
