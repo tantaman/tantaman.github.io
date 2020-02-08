18e2ba95c4e02e1e18ea98b8271bedabf0f4a56c
tantaman <matt.crinklaw@gmail.com>
2020-02-08 14:53:09 -0500

    Add routing

--- svelte-site/src/App.svelte
+++ svelte-site/src/App.svelte
@@ -1,29 +1,12 @@
 <script>
+  import { Router, Route } from "svelte-routing";
+  import Home from "./Home.svelte";
 
+  export let url = "";
 </script>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
-<main>
-  <h1>Tantamanlands</h1>
-</main>
+<Router {url}>
+  <Route path="/">
+    <Home />
+  </Route>
+</Router>
