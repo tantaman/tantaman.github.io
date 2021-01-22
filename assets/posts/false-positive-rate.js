e6c178fa0e79db2742aff2332ca42b604cf64302
Matt Crinklaw-Vogt <mlaw@fb.com>
2021-01-22 10:11:06 -0500

    more drawing

---
+++ assets/posts/false-positive-rate.js
@@ -0,0 +1,31 @@
+const PersonStates = {
+  DEFAULT = 'DEFAULT',
+  FALSE_POSITIVE = 'false_positive',
+  TRUE_POSITIVE = 'true_positive',
+  FALSE_NEGATIVE = 'false_negative',
+  TRUE_NEGATIVE = 'true_negative',
+};
+
+function person(state) {
+  return (
+    `<span class="person">
+      <div class="head"></div>
+      <div class="body"></div>
+      <div class="left-arm-divider"></div>
+      <div class="right-arm-divider"></div>
+    </span>`
+  );
+}
+
+function pop_readout(size) {
+  return (
+    `<center class="pop-readout">
+      <span>
+        <span class="pop-num">
+        ${size.toLocaleString()}
+        </span>
+        ${person(PersonStates.DEFAULT)}
+      </span>
+    </center>`
+  );
+}
