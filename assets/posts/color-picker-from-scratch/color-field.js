27df3efee74558aa6069d1d75d486a5a0bcb29d8
tantaman <matt.crinklaw@gmail.com>
2021-12-28 09:58:52 -0500

    rgb to hsv

--- assets/posts/color-picker-from-scratch/color-field.js
+++ assets/posts/color-picker-from-scratch/color-field.js
@@ -1,5 +1,7 @@
 import { publisher, atom } from '/assets/js/publisher.js';
 
+// https://www.rapidtables.com/convert/color/hsv-to-rgb.html
+
 function hsvToRgb(h, s, v) {
   const c = v * s;
   const x = c * (1 - Math.abs(((h / 60) % 2) - 1));
@@ -38,6 +40,35 @@ function hsvToRgb(h, s, v) {
   return [(rp + m) * 255, (gp + m) * 255, (bp + m) * 255];
 }
 
+function rgbToHsv(r, g, b) {
+  const [rp, gp, bp] = [r, g, b].map((x) => x / 255.0);
+  const cMax = Math.max(rp, gp, bp);
+  const cMin = Math.min(rp, gp, bp);
+  const delta = cMax - cMin;
+
+  let hue = 0;
+  if (delta === 0) {
+    hue = 0;
+  } else if (cMax === rp) {
+    hue = 60 * (((gp - bp) / delta) % 6);
+  } else if (cMax === gp) {
+    hue = 60 * ((bp - rp) / delta + 2);
+  } else if (cMax === bp) {
+    hue = 60 * ((rp - gp) / delta + 4);
+  }
+
+  let sat = 0;
+  if (cMax === 0) {
+    sat = 0;
+  } else {
+    sat = delta / cMax;
+  }
+
+  const value = cMax;
+
+  return [hue, sat, value];
+}
+
 function rgbToHex(r, g, b) {
   const pad = (v) => (v.length === 1 ? '0' + v : v);
   return '#' + pad(r.toString(16)) + pad(g.toString(16)) + pad(b.toString(16));
@@ -59,6 +90,13 @@ const templates = {
     </div>
   </div>
   `,
+  addingHue: (props) => `
+  <div class="colorField" style="width:${fieldSize.width}px; height: ${
+    fieldSize.height
+  }px;">
+    <div class="stretch" style="${'background: ' + props.background}"></div>
+  </div>
+  `,
 };
 
 const state = atom({ background: rgbToHex(...hsvToRgb(0, 1, 1)) });
@@ -68,5 +106,7 @@ const state = atom({ background: rgbToHex(...hsvToRgb(0, 1, 1)) });
   html(templates.colorField, state);
 }
 
-console.log(hsvToRgb(0, 1, 1));
-console.log(rgbToHex(...hsvToRgb(0, 1, 1)));
+{
+  const { _md, html } = publisher(document.getElementById('showing-hue-ex'));
+  html(templates.addingHue, state);
+}
