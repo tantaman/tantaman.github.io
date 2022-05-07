9b9a7b504fe08cd965a562bee5b635c501c8b06b
Matt <1009003+tantaman@users.noreply.github.com>
2022-05-07 16:55:35 -0400

    import assets, start using figure component

---
+++ src/components/Figure.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Figure({
+  description,
+  source,
+  source_url,
+  url,
+}: {
+  description: string;
+  source: string;
+  source_url: string;
+  url: string;
+}) {
+  return (
+    <figure className="image" style={{ textAlign: 'center' }}>
+      <img src={url} alt={description} />
+      <figcaption>
+        <i>
+          {description}
+          {source ? <a href={source_url}>{source}</a> : null}
+        </i>
+      </figcaption>
+    </figure>
+  );
+}
