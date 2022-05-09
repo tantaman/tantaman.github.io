bdedf484a8eeaf36de6d5a3ce79c454579e6d0f3
Matt <1009003+tantaman@users.noreply.github.com>
2022-05-09 15:56:54 -0400

    write built files

--- compiler/build.js
+++ compiler/build.js
@@ -1,18 +1,28 @@
 import { bundleMDX } from 'mdx-bundler';
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-async function build() {
-  const files = await fs.promises.readdir('./site/blog');
-  return await Promise.all(
-    files.map((file) =>
-      bundleMDX({
-        file: path.resolve('./site/blog/' + file),
-        cwd: path.resolve('./site/blog'),
+async function build(collection) {
+  const dest = './dist/' + collection;
+  const files = await fs.promises.readdir('./site/' + collection);
+  const artifacts = await Promise.all(
+    files.map(async (file) => [
+      dest + '/' + file,
+      await bundleMDX({
+        file: path.resolve('./site/' + collection + '/' + file),
+        cwd: path.resolve('./site/' + collection),
       }),
+    ]),
+  );
+
+  console.log(artifacts);
+
+  await fs.promises.mkdir(dest, { recursive: true });
+  await Promise.all(
+    artifacts.map(
+      async ([path, a]) => await fs.promises.writeFile(path, JSON.stringify(a)),
     ),
   );
 }
 
-const builtPosts = await build();
+await build('blog');
