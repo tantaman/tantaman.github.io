ba4521a3fc767c5bc0b60e87f3d973d70d8535b4
Matt <1009003+tantaman@users.noreply.github.com>
2022-05-10 14:50:07 -0400

    fetch the index in the blog

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Blog() {
+  const [index, setIndex] = useState<{ [key: string]: {} }>();
+  useEffect(() => {
+    fetch('/built/blog/index.json').then(async (response) => {
+      const json = await response.json();
+      setIndex(json);
+    });
+  }, []);
+  console.log(index);
   return <div>Blog</div>;
 }
