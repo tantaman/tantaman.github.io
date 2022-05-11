dc745eff93315fb0bd95b41da574e71e3893d287
Matt <1009003+tantaman@users.noreply.github.com>
2022-05-11 15:31:42 -0400

    rm existing src -- now that we're full static going a new direction

--- src/error/Oops.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Oops({ e }: { e: any }) {
-  return <div>{e}</div>;
-}
--- src/support/Header.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { cls } from './header.css';
-
-export default function Header() {
-  return <header className={cls}>Tantamanlands</header>;
-}
