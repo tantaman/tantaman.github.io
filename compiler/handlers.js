2093f39d5684041a888033edb33fba855526cf62
Matt <1009003+tantaman@users.noreply.github.com>
2022-05-10 22:26:54 -0400

    md converter at build time, wikilinks plugins

--- compiler/handlers.js
+++ compiler/handlers.js
@@ -1,6 +1,16 @@
 import { bundleMDX } from 'mdx-bundler';
 import fs from 'fs';
 
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkGfm from 'remark-gfm';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import remarkWikiLink from 'remark-wiki-link';
+import extractFromtmatter from 'remark-extract-frontmatter';
+import yaml from 'yaml';
+
 export default {
   async mdx(file, cwd) {
     const ret = await bundleMDX({
@@ -12,10 +22,19 @@ export default {
   },
 
   async md(file, cwd) {
-    const code = await fs.promises.readFile(file, { encoding: 'utf8' });
+    const contents = await fs.promises.readFile(file, { encoding: 'utf8' });
+    const parsed = await unified()
+      .use(remarkParse)
+      .use(remarkFrontmatter)
+      .use(extractFromtmatter, { yaml: yaml.parse })
+      .use(remarkGfm)
+      .use(remarkRehype)
+      .use(rehypeStringify)
+      .use(remarkWikiLink)
+      .process(contents);
     return {
-      code,
-      frontmatter: {},
+      code: parsed.toString(),
+      frontmatter: parsed.data,
     };
   },
 
