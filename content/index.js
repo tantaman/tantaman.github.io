876dc24729c3bb798780fcecf178f440f62cccae
Matt <1009003+tantaman@users.noreply.github.com>
2022-05-13 11:50:23 -0400

    add published date

--- content/index.js
+++ content/index.js
@@ -1,9 +1,7 @@
 import { doc, meta, layout, rehypeDocument } from '@tantaman/sitecompiler';
-import { h } from 'hastscript';
 import rehypeStringify from 'rehype-stringify';
 import { unified } from 'unified';
 import rehypeMeta from 'rehype-meta';
-import { toHtml } from 'hast-util-to-html';
 import rehypeParse from 'rehype-parse';
 
 // TODO: put this thru the unified pipeline like markdown?
