1f0f2802b5dc522d81f3cb12231a7fc0b2d0adc4
Matt Wonlaw <mlaw@roci.dev>
2025-07-26 06:30:26 -0400

    spawn to get sub-process io

--- packages/compiler/src/layouts/layouts.js
+++ packages/compiler/src/layouts/layouts.js
@@ -7,6 +7,7 @@ export const layouts = {
 };
 
 export default function layout() {
+  console.log('Using layout middlewar SDFSDFe');
   return (tree, file) => {
     layouts[file.data?.matter?.layout || 'default'](tree, file);
   };
