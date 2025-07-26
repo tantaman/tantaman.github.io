b0af5b4b64592f6b773ddf8f0d1449bbe2b9c43b
Matt Wonlaw <mlaw@roci.dev>
2025-07-26 13:19:49 -0400

    update mermaid styles for dark bg

--- packages/frontend/src/components/Mermaid.js
+++ packages/frontend/src/components/Mermaid.js
@@ -1,12 +1,15 @@
 import React from 'https://esm.sh/react';
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs';
+mermaid.initialize({
+  theme: 'dark',
+});
 
 export default function Mermaid({ id, chart }) {
   return (
     <div
-      ref={(node) => {
-        mermaid.mermaidAPI.render(id, chart, (svgCode) => {
-          node.innerHTML = svgCode;
-        });
+      ref={async (node) => {
+        const { svg } = await mermaid.render(id, chart);
+        node.innerHTML = svg;
       }}
     ></div>
   );
